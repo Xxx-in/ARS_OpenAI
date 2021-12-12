4c885df4e0f832be4289b039404dd1bf35139d58
zhixin <limzhixin.lzx@gmail.com>
2021-12-12 17:12:13 +0800

    Edit repeat.py file to loop new _ff2 files

--- repeat.py
+++ repeat.py
@@ -1,9 +1,11 @@
-import os 
+import subprocess 
 for i in range(25):
-    os.system("cartpole_v0_qlearning.py")
-    os.system("cartpole_v0_sarsa.py")    
-    os.system("cartpole_v0_gene_meanWeightCO_ff1.py")
-    os.system("cartpole_v0_gene_qWeightCO_ff1.py")
-    os.system("cartpole_v0_gene_randWeightCO_ff1.py")
-    os.system("cartpole_v0_gene_uniformCO_ff1.py")
+    subprocess.call(['python', "cartpole_v0_qlearning.py"])
+    subprocess.call(['python', "cartpole_v0_sarsa.py"])    
+    subprocess.call(['python', "cartpole_v0_gene_meanWeightCO_ff1.py"])
+    subprocess.call(['python', "cartpole_v0_gene_randWeightCO_ff1.py"])
+    subprocess.call(['python', "cartpole_v0_gene_uniformCO_ff1.py"])
+    subprocess.call(['python', "cartpole_v0_gene_meanWeightCO_ff2.py"])
+    subprocess.call(['python', "cartpole_v0_gene_randWeightCO_ff2.py"])
+    subprocess.call(['python', "cartpole_v0_gene_uniformCO_ff2.py"])
 print ('done')
